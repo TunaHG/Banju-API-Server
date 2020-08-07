04f497a1639e12f9b4c8a07eb104e45fbf504c54
김하균 <asdf0185@naver.com>
2020-08-07 17:46:13 +0900

    Test: Set API Server

--- TestAPI2/app.js
+++ TestAPI2/app.js
@@ -1,5 +1,23 @@
 const express = require('express');
-const api = require('./api');
+const bodyParser = require('body-parser');
+
+const musicreg = require('./api/v1/musicreg');
+const playmeta = require('./api/v1/playmeta');
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('express start');
+});
+
+// Routing
+app.use('/musicreg', musicreg);
+app.use('/playmeta', playmeta);
+
+app.listen(port, () => {
+    console.log('Express listening on port', port);
+});
