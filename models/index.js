0c9ffa5887b9a85f9ca55b31deaf6578ec754b6a
김하균 <asdf0185@naver.com>
2020-09-09 10:23:58 +0900

    DB: Set Sequelize Association

--- models/index.js
+++ models/index.js
@@ -10,7 +10,22 @@ db.Sequelize = Sequelize;
 db.Users = require('./Users')(sequelize, Sequelize);
 db.Banjus = require('./Banjus')(sequelize, Sequelize);
 db.Practices = require('./Practices')(sequelize, Sequelize);
+db.Posts = require('./Posts')(sequelize, Sequelize);
+db.Comments = require('./Comments')(sequelize, Sequelize);
+db.Follows = require('./Follows')(sequelize, Sequelize);
 
-// db.Users.hasMany()
+// Database Associations
+db.Users.hasMany(db.Banjus);
+db.Banjus.belongsTo(db.Users);
+db.Users.hasMany(db.Practices);
+db.Practices.belongsTo(db.Users);
+db.Banjus.hasMany(db.Practices);
+db.Practices.belongsTo(db.Banjus);
+db.Users.hasMany(db.Posts);
+db.Posts.belongsTo(db.Users);
+db.Posts.hasMany(db.Comments);
+db.Comments.belongsTo(db.Posts);
+db.Users.hasMany(db.Comments);
+db.Comments.belongsTo(db.Users);
 
 module.exports = db;
