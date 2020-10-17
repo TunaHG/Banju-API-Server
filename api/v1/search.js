b08d061a25ad1d302f287f42dffb343d7c95636d
김하균 <asdf0185@naver.com>
2020-10-17 18:15:33 +0900

    Feat: Update search API about order, filter

--- api/v1/search.js
+++ api/v1/search.js
@@ -2,11 +2,20 @@ const express = require('express');
 const config = require('../../config/config');
 const axios = require('axios');
 const searchService = require('../../services/searchService');
+const playmetaService = require('../../services/playmetaService');
 
 const router = express.Router();
 
-router.get('/:keyword', (req, res) => {
-    const keyword = req.params.keyword;
+// http://api.dailybanju.com/search/다이너마이트/date/
+router.get('/', (req, res) => {
+    const keyword = req.body.keyword;
+    // order: date, rating, relevance, title, videoCount, viewCount (default: relevance)
+    const order = req.body.order;
+    // publishedAfter: datetime 1970-01-01T00:00:00Z
+    const publishedAfter = req.body.publishedAfter;
+    // videoDuration: any, long(>20m), medium(>4m, <20m), short(<4m)
+    const videoDuration = req.body.videoDuration;
+    const pageToken = req.body.pageToken;
 
     const option = {
         method: 'GET',
@@ -17,10 +26,28 @@ router.get('/:keyword', (req, res) => {
             type: 'video',
             key: config.googleapikey,
             maxResults: '5',
-            order: 'relevance',
             topicId: '/m/04rlf'
         }
     }
+
+    if (order !== undefined) {
+        option.params.order = order;
+    }
+    if (publishedAfter !== undefined) {
+        const datetime = new Date();
+        datetime.setDate(datetime.getDate() - publishedAfter);
+        option.params.publishedAfter = datetime;
+    }
+    if (videoDuration !== undefined) {
+        option.params.videoDuration = videoDuration;
+    }
+    if (typeof pageToken !== 'undefined') {
+        option.params.pageToken = pageToken;
+    }
+
+    // if (pageToken !== undefined) {
+    //     option.params.pageToken = pageToken;
+    // }
 
     axios.request(option)
         .then(async ({ data }) => {
@@ -38,7 +65,7 @@ router.get('/:keyword', (req, res) => {
                 tmp.id = element.id.videoId;
                 tmp.title = element.snippet.title;
                 tmp.thumbnail = element.snippet.thumbnails.default;
-                await searchService.findBanju(tmp.id)
+                await playmetaService.findBanju(tmp.id)
                     .then((result) => {
                         // TODO: Banju의 Scale 추가
                         if (result === 0) {
