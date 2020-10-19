4a1bb217006ec340b81e257d1dd76e92e129832d
김하균 <asdf0185@naver.com>
2020-10-19 18:55:02 +0900

    Sentry: Init Sentry Setting

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config/config');
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
 
 const playmeta = require('./api/v1/playmeta');
 const user = require('./api/v1/user');
@@ -11,6 +13,21 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(config.databaseurl);
 const models = require('./db/models');
 
+const app = express();
+const port = config.port;
+
+// Sentry Setting
+Sentry.init({
+    dsn: config.sentrydsn,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 models.sequelize.sync({})
     .then(() => {
         console.log("DB sync Success");
@@ -19,9 +36,6 @@ models.sequelize.sync({})
         console.log('DB sync Failed');
         console.log("Error: ", err);
     });
-
-const app = express();
-const port = config.port;
 
 const passport = require('passport');
 const passportConfig = require('./config/passport');
@@ -44,6 +58,16 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
 app.use('/playmeta', playmeta);
 app.use('/user', user);
 app.use('/search', search);
+
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+});
 
 app.listen(port, (err) => {
     if (err) {
